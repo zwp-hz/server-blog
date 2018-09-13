a878c2134677c971b51551b4ba6e8b8855b9c2e4
朱为鹏 <1453928106@qq.com>
2018-09-13 13:55:10 +0800

    添加  cross-env 包

--- server/server.js
+++ server/server.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const app = express();
 const api = require('./api');
-const db = require('./db');
 
 app.use(api);
-app.listen(8989, function () {
-  console.log('Visit http://localhost:8989')
+app.listen(8989, function() {
+    console.log('Visit http://localhost:8989')
 });
