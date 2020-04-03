021c3d10f8adb3036fdb86d2ae475dcbd7f9638b
朱为鹏 <1453928106@qq.com>
2020-04-03 17:23:44 +0800

    添加密钥

--- server/common.js
+++ server/common.js
@@ -25,9 +25,8 @@ const upload = multer({
 
 const sendEmail = data => {
   if (process.env.NODE_ENV === "production") {
-    
-    const AccessKeyId = "";
-    const AccessKeySecret = "";
+    const AccessKeyId = "LTAIoz8hTvYb4xlC";
+    const AccessKeySecret = "ybCZYywWaeHBFiA2ikoS1OjXfKzkxh";
     const DM = new ALY.DM({
       accessKeyId: AccessKeyId,
       secretAccessKey: AccessKeySecret,
